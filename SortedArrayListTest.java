78e1e15183e4245a20d34603b9f563abfc1d546d
vnguyen155 <nguyentrankhanhvan0@gmail.com>
2026-09-17 15:48:45 -0600

    update some tests

--- SortedArrayListTest.java
+++ SortedArrayListTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Collection;
 import java.util.NoSuchElementException;
@@ -17,33 +19,86 @@ import java.util.NoSuchElementException;
  */
 public class SortedArrayListTest {
 	private SortedArrayList<Integer> list;
-	
+
 	@BeforeEach
 	void setUp() {
 		list = new SortedArrayList<>();
 	}
-	
+
 	/*
 	 * Tests
 	 */
+
 	@Test
 	public void newListEmpty() {
 		assertTrue(list.isEmpty());
 		assertEquals(0, list.size());
 	}
-	
+
+	@Test
+	public void comparatorReverseOrder() {
+		SortedArrayList<Integer> reversed = new SortedArrayList<>(Comparator.reverseOrder());
+		reversed.insert(2);
+		reversed.insert(4);
+		reversed.insert(6);
+		assertEquals(6, reversed.min());
+		assertEquals(2, reversed.max());
+	}
+
 	@Test
 	public void minThrowsWhenEmpty() {
 		assertThrows(NoSuchElementException.class, () -> list.min());
 	}
-	
+
 	@Test
 	public void maxThrowsWhenEmpty() {
 		assertThrows(NoSuchElementException.class, () -> list.max());
 	}
-	
+
 	@Test
 	public void medianThrowsWhenEmpty() {
 		assertThrows(NoSuchElementException.class, () -> list.median());
 	}
+
+	@Test
+	public void medianOddSize() {
+		list.insert(6);
+		list.insert(8);
+		list.insert(9);
+
+		assertEquals(8, list.median());
+
+	}
+
+	@Test
+	public void medianEvenSize() {
+		list.insert(6);
+		list.insert(3);
+		list.insert(8);
+		list.insert(9);
+
+		assertEquals(8, list.median());
+	}
+
+	@Test
+	public void insertKeepsListSorted() {
+		list.insert(5);
+		list.insert(4);
+		list.insert(6);
+		list.insert(7);
+
+		assertEquals(7, list.max());
+		assertEquals(6, list.median());
+		assertEquals(4, list.min());
+	}
+
+	@Test
+	public void sizeIncreasesWithEachInsert() {
+		assertEquals(0, list.size());
+		list.insert(3);
+		assertEquals(1, list.size());
+		list.insert(9);
+		assertEquals(2, list.size());
+	}
+
 }
